1b0e1d323bbe4f719de4e95fa82baf84426f148d
nakayuu07 <nakayuu07@gmail.com>
2019-09-24 09:58:30 +0900

    template_literal

---
+++ template_literal/space.js
@@ -0,0 +1,15 @@
+// let name = "太郎"
+// let age = 18;
+// // \nなしで改行など(spaceやtab等々)できる
+// let message = `私の名前は${name}で
+// ${age}です`
+// console.log(message)
+
+// // jsの記法なら展開できる
+// // let message = `私の名前は${"桃" + name}で${age * 2}才です`
+// // message
+
+// //関数もいける
+// let foods = ["卵", "納豆", "豆腐"]
+// let message = `私が好きな食べ物は、${foods.join(",")}です`
+// message
---
+++ template_literal/template_with_tag.js
@@ -0,0 +1,20 @@
+// function output() {
+//   console.log(arguments)  // { [Iterator]  0: 'a', 1: 'b', [Symbol(Symbol.iterator)]: [λ: values] } 
+// }
+
+// output("a", "b")
+
+// function output() {
+//   console.log(arguments) // [Iterator]  0: [ '"a", "b"' ], [Symbol(Symbol.iterator)]: [λ: values] } 
+// }
+
+// output`"a", "b"`
+
+//これは謎
+// https://www.udemy.com/course/ecmascript-es6/learn/lecture/10143098#overview
+// function output(string, ...values) {
+//   console.log(string)
+//   console.log(values)
+// }
+
+// output`"a", "b"`
