1af6860e2bc67c9b14cdc01320826a1a13335705
nakayuu07 <nakayuu07@gmail.com>
2019-09-24 09:56:22 +0900

    const let

---
+++ const_let/const.js
@@ -0,0 +1,13 @@
+//const再代入不可
+// const a = "a"
+// const a = "z"
+
+//オブジェクトや配列に追加・削除はできる
+// const obj = {}
+// obj["a"] = "1"
+// obj {a: 1}
+
+// const arr = []
+// arr.push(1)
+// arr[1] [1]
+// arr.pop() []
